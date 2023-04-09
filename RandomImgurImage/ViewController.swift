4989fb76d4bfacdd38ae5697ff277a2919da1853
qwezxcdegen <ifomstep@icloud.com>
2023-04-09 19:34:41 +0500

    Finish

--- RandomImgurImage/ViewController.swift
+++ RandomImgurImage/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    private let chars = "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func generatePressed() {
+        DispatchQueue.main.async {
+            self.imageView.image = nil
+            self.activityIndicator.startAnimating()
+        }
+        var text = ""
+        for _ in 1...5 {
+            text += String(chars.randomElement() ?? " ")
+        }
+        let url = "https://i.imgur.com/" + text + ".jpg"
+        print(url)
+        guard let url = URL(string: url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data, let image = UIImage(data: data) else { return }
+            if image.size.height == 81 && image.size.width == 161 {
+                self.generatePressed()
+            } else {
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+        }.resume()
+    }
+    
+    @IBAction func downloadPressed() {
+        guard let image = imageView.image else { return }
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        let ac = UIAlertController(title: "OK", message: "Saved", preferredStyle: .alert)
+        present(ac, animated: true)
+        dismiss(animated: true)
+    }
+    
 }
 
